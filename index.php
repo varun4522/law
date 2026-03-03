a3948422f5bfcd2e8d35871c56fe5779a5ee3f0b
Varun <varunking934@gmail.com>
2026-03-03 08:19:37 +0530

    ss

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Law Application</title>
+    <title>Login - Law hhApplication</title>
     <style>
         * {
             margin: 0;
