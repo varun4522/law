b7127f19c521de3cea63a6510f2dd0c570de4065
Varun <varunking934@gmail.com>
2026-03-04 18:25:12 +0530

    restart

--- lib/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'db.php';
-
-startSession();
-session_destroy();
-
-header('Content-Type: application/json');
-echo json_encode(['success' => true, 'message' => 'Logged out successfully']);
-?>
